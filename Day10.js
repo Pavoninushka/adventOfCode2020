6b05c15bb2f31eb349e33403255844b8d24401c3
Galina Karateeva <karateewa.galina@yandex.ru>
2020-12-28 20:15:38 +0100

    Solution for Day10.Part2

--- Day10.js
+++ Day10.js
@@ -6,20 +6,55 @@ let data = file.toString();
 let task = data.split("\n").map(number => Number(number)).sort(function(a,b){
     return a - b;
 });
+task.unshift(0);
 
 console.log(task);
 
 const part1 = () => {
     let oneJolt = 1;
     let threeJolt = 1;
-for (let i = 0; i < task.length; i++) {
-
-if (task[i + 1] - task[i] === 1) {
-    oneJolt++;
-} else if (task[i + 1] - task[i] === 3) {
-    threeJolt++;
+    for (let i = 0; i < task.length; i++) {
+        if (task[i + 1] - task[i] === 1) {
+            oneJolt++;
+        } else if (task[i + 1] - task[i] === 3) {
+            threeJolt++;
+        }
     }
-} return oneJolt * threeJolt;
+    return oneJolt * threeJolt;
 }
 
 console.log(`part 1 solution: ${part1()}`);
+
+let memory = new Map();
+
+const sumOfEnds = (i) => {
+    if (memory.has(i)) {
+        return memory.get(i);
+    }
+
+    if (i === task.length - 1)  {
+        return 1;
+    }
+
+    let result = 0
+
+    if (task[i + 1] - task[i] <= 3) {
+        result += sumOfEnds(i + 1);
+    }
+    if (task[i + 2] - task[i] <= 3) {
+        result += sumOfEnds(i + 2);
+    }
+    if (task[i + 3] - task[i] <= 3) {
+        result += sumOfEnds(i + 3);
+    }
+
+    memory.set(i, result);
+
+    return result;
+}
+
+const part2 = () => {
+   return sumOfEnds(0);
+}
+
+console.log(`part 2 solution: ${part2()}`);
