443f61af3fa08c7733b338799c10997694d303f0
Galina Karateeva <karateewa.galina@yandex.ru>
2020-12-29 18:00:18 +0100

    Solution for Day11.Part1

---
+++ Day11.js
@@ -0,0 +1,97 @@
+let fs = require('fs');
+
+let file = fs.readFileSync('Day11Input.txt', 'utf8');
+let data = file.toString();
+
+const processData = (line) => {
+    let rows = [];
+
+    for (let allRows of line.split("\n")) {
+        let row = allRows.split("");
+        rows.push(row);
+    }
+    return rows;
+}
+
+const SHIFTS = [
+    [-1, -1],
+    [-1, 0],
+    [-1, +1],
+    [0, +1],
+    [+1, +1],
+    [+1, 0],
+    [+1, -1],
+    [0, -1],
+];
+
+const countOccupiedAdjacent = (i, j, matrix) => {
+    let result = 0;
+
+    for (let [shiftI, shiftJ] of SHIFTS) {
+        let newI = i + shiftI;
+        let newJ = j + shiftJ;
+        if (newI >= 0 && newI < matrix.length && matrix[newI][newJ] === "#") {
+            result++;
+        }
+    }
+    return result;
+}
+
+const nextState = (matrix) => {
+    let newState = [];
+    for (let i = 0; i < matrix.length; i++) {
+        let newRow = [];
+        for (let j = 0; j < matrix[i].length; j++) {
+            let currentState = matrix[i][j];
+            let nextState = currentState;
+
+            if (currentState === "L" && countOccupiedAdjacent(i, j, matrix) === 0) {
+                nextState = "#";
+            } else if (currentState === "#" && countOccupiedAdjacent(i, j, matrix) >= 4) {
+                nextState = "L";
+            }
+            newRow.push(nextState);
+        }
+        newState.push(newRow);
+    }
+    return newState;
+}
+
+const isEqualMatrix = (matrix1, matrix2) => {
+    for (let i = 0; i < matrix1.length; i++) {
+        for (let j = 0; j < matrix1[i].length; j++) {
+            if (matrix1[i][j] !== matrix2[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+const countOccupied = (matrix) => {
+    let sumOfOccupied = 0;
+    for (let row of matrix) {
+        for (let seat of row) {
+            if (seat === "#") {
+                sumOfOccupied++;
+            }
+        }
+    }
+    return sumOfOccupied;
+}
+
+const part1 = () => {
+    let currentMatrix = processData(data);
+
+    while (true) {
+        let nextMatrix = nextState(currentMatrix);
+        if (isEqualMatrix(currentMatrix, nextMatrix)) {
+            break;
+        }
+        currentMatrix = nextMatrix;
+    }
+    return countOccupied(currentMatrix);
+}
+
+console.log(`part 1 solution: ${part1()}`);
+
